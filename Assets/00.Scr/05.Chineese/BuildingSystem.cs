01ce20586896ea94940694e94673a35619868bb6
minho0516 <pmhminho0516@gmail.com>
2024-07-10 20:17:54 +0900

    .
    ..

--- Assets/00.Scr/05.Chineese/BuildingSystem.cs
+++ Assets/00.Scr/05.Chineese/BuildingSystem.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class BuildingSystem : MonoBehaviour
@@ -12,11 +13,17 @@ public class BuildingSystem : MonoBehaviour
     public Transform cam;
     public RaycastHit hit;
     public LayerMask layer;
+    public LayerMask whatIsBuilding;
 
     public float offset = 1.0f;
     public float gridSize = 1.0f;
 
     public bool isBuilding = true;
+
+    public MCFace dir;
+    public ObjectSort objectSorts;
+
+    private int currentPosPlusY;
 
 
     private void Awake()
@@ -58,6 +65,22 @@ public class BuildingSystem : MonoBehaviour
             Debug.Log("2·Î ¹Ù²î¾ú³ë");
             ChangeCurrentBuilding(1);
         }
+        if (Input.GetKeyDown("3"))
+        {
+            Debug.Log("3·Î ¹Ù²î¾ú³ë");
+            ChangeCurrentBuilding(2);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            Debug.Log("È®³»·Á°¬´Ù°¡");
+            currentPosPlusY--;
+        }
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("È®¿Ã¶ó°¬´Ù°¡ ");
+            currentPosPlusY++;
+        }
     }
 
     public void SwitchCurrentBuilding()
@@ -95,12 +118,18 @@ public class BuildingSystem : MonoBehaviour
 
     public void ShowPreview(RaycastHit hit)
     {
+        if(objectSorts == ObjectSort.Floor)
+            
         currentPos = hit.point;
         currentPos -= Vector3.one * offset;
         currentPos /= gridSize;
         currentPos = new Vector3(Mathf.Round(currentPos.x), Mathf.Round(currentPos.y), Mathf.Round(currentPos.z));
         currentPos *= gridSize;
         currentPos += Vector3.one * offset;
+
+        //³»²¨ ¤¾¤¾
+        currentPos += new Vector3(0, currentPosPlusY, 0);
+
         currentPreview.position = currentPos;
 
         if (Input.GetMouseButtonDown(1))
@@ -119,6 +148,38 @@ public class BuildingSystem : MonoBehaviour
             Instantiate(currentObject.prefab, currentPreview.position, Quaternion.Euler(currentRot));
         }
     }
+
+    public static MCFace GetHitFace(RaycastHit hit)
+    {
+        Vector3 inComingVec = hit.normal - Vector3.up;
+
+        if(inComingVec == new Vector3(0, -1, -1))
+        {
+            return MCFace.South;
+        }
+        if (inComingVec == new Vector3(0, -1, 1))
+        {
+            return MCFace.North;
+        }
+        if (inComingVec == new Vector3(0, 0, 0))
+        {
+            return MCFace.Up;
+        }
+        if (inComingVec == new Vector3(1, 1, 1))
+        {
+            return MCFace.Down;
+        }
+        if (inComingVec == new Vector3(-1, -1, 0))
+        {
+            return MCFace.West;
+        }
+        if (inComingVec == new Vector3(1, -1, 0))
+        {
+            return MCFace.East;
+        }
+
+        return MCFace.None;
+    }
 }
 
 [System.Serializable]
@@ -127,5 +188,23 @@ public class buildObject
     public string name;
     public GameObject prefab;
     public GameObject preview;
+    public ObjectSort objectSorts;
     public int gold;
+}
+
+public enum MCFace
+{
+    None,
+    Up,
+    Down,
+    East,
+    West,
+    North,
+    South
+}
+
+public enum ObjectSort
+{
+    Floor,
+    Wall
 }
