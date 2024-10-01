d20b2e762fec74da5bf97ef9109e1c9274884560
Baetaekyung <tkwayi2129@gmail.com>
2024-10-02 00:37:56 +0900

    fixing inventory UI
    인벤토리 유아이 고치는 중입니다

--- Assets/00.Scr/03.TK/UI/Inventory/CraftItemBar.cs
+++ Assets/00.Scr/03.TK/UI/Inventory/CraftItemBar.cs
@@ -4,7 +4,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CraftBlueprint : MonoBehaviour
+public class CraftItemBar : MonoBehaviour
 {
     [SerializeField] private Image ingredientIcon;
     [SerializeField] private TextMeshProUGUI resultItemName;
