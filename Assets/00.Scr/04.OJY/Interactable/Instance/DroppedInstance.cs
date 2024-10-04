b34c29e9e4cb15905cce44796b55a2b86aeef57e
Bash_Hydrogen102 <baesh248@gmail.com>
2024-10-04 17:21:52 +0900

    ItemDropFix

--- Assets/00.Scr/04.OJY/Interactable/Instance/DroppedInstance.cs
+++ Assets/00.Scr/04.OJY/Interactable/Instance/DroppedInstance.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class DroppedInstance : MonoBehaviour, IInteractable
 {
     [SerializeField] private SO_Item soitem;
+    void OnEnable()
+    {
+        transform.SetParent(null);
+    }
     public void OnPlayerInteract(Player player)
     {
         InventoryUI.Instance.PlayerInventory.TryAddItemToInventory(soitem);
