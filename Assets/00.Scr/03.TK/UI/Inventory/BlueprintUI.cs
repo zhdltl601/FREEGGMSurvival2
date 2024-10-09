f98e242a4b77384b608c541e7438d669e2a467ca
Baetaekyung <tkwayi2129@gmail.com>
2024-10-09 20:38:15 +0900

    Subtract Item

--- Assets/00.Scr/03.TK/UI/Inventory/BlueprintUI.cs
+++ Assets/00.Scr/03.TK/UI/Inventory/BlueprintUI.cs
@@ -1,4 +1,5 @@
 using DG.Tweening;
+using System.Collections.Generic;
 using System.Text;
 using TMPro;
 using UnityEngine;
@@ -16,6 +17,9 @@ public class BlueprintUI : MonoBehaviour, IPointerDownHandler
     private SO_Item resultItem;
     private Inventory inventory;
     private BlueprintViewer bpViewer;
+
+    private Dictionary<string, SO_Item> subtractItems = new();
+    private Dictionary<string, int> subtractItemAmount = new();
 
     public bool canCraft = false;
 
@@ -59,14 +63,21 @@ public class BlueprintUI : MonoBehaviour, IPointerDownHandler
             BlueprintViewer bpViewer = BlueprintViewer.Instance;
             sb.Clear();
             int currentAmount = 0;
+            int needAmount = item.amount;
             if (bpViewer.currentItemSetting.ContainsKey(item.so_item.GetName))
+            {
                 currentAmount = bpViewer.currentItemSetting[item.so_item.GetName];
+                subtractItems.Add(item.so_item.GetName, item.so_item);
+                if(currentAmount >= needAmount)
+                {
+                    subtractItemAmount.Add(item.so_item.GetName, needAmount);
+                }
+            }
             else
                 currentAmount = 0;
 
             sb.Append(currentAmount.ToString());
             sb.Append("/");
-            int needAmount = item.amount;
             sb.Append(needAmount);
 
             if (needAmount != 0)
@@ -88,6 +99,19 @@ public class BlueprintUI : MonoBehaviour, IPointerDownHandler
         if (canCraft)
         {
             inventory.TryAddItemToInventory(resultItem);
+
+            #region Subtract ingredient items
+
+            foreach(var item in subtractItems)
+            {
+                inventory.TrySubtractFromInventory(item.Value, subtractItemAmount[item.Key]);
+                Debug.Log($"Subtract {item.Value.GetName} item amount {subtractItemAmount[item.Key]} from inventory");
+            }
+            subtractItems.Clear();
+            subtractItemAmount.Clear();
+
+            #endregion
+
             BlueprintViewer.Instance.RemoveAllByCrafting();
             Debug.Log($"Success! item {resultItem} is add in inventory");
         }
